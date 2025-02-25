9b8e4a901484b6f73df93350cd7b53f1882893e6
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-02-26 00:35:21 +0600

    change

--- array/js/script.js
+++ array/js/script.js
@@ -1,5 +1,25 @@
+//splice method
+// let num = [1,2,3,4,5,6,7,8];
+// //num.splice(2,3);//remove 3,4,5
+// num.splice(2,3,10,11,12);
+// console.log(num); //output: 1,2,10,11,12,6,7,8
 
-let num = [1,2,3,4,5,6,7,8];
-//num.splice(2,3);//remove 3,4,5
-num.splice(2,3,10,11,12);
-console.log(num); //output: 1,2,10,11,12,6,7,8
+
+// //slice method
+// let num = [1,2,3,4,5,6,7,8];
+// //num.splice(2,3);//remove 3,4,5
+// let x = num.slice(1);
+// console.log(x); 
+// console.log(num); 
+
+
+// let x = [2,4,1,6,7,9,2,7];
+// x.sort();
+// console.log(x);
+
+
+// let x = [3,2,6,5,9,4,7,1,8];
+// x.sort();
+// // x.reverse();
+
+// console.log(x);

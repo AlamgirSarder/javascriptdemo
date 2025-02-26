11a2192015327e2807b84b341b02009ddfb39f5f
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-02-27 00:28:58 +0600

    push pop shift unshift slice splice

--- array/js/script.js
+++ array/js/script.js
@@ -1,7 +1,9 @@
 //push()
 //pop()
-//shift
-//unshift
+//shift()
+//unshift()
+//slice()
+//splice()
 
 
 let arr = [1,2,3,4,5,6,7,8];
@@ -15,6 +17,17 @@ console.log(arr);
 
 arr.shift();
 console.log(arr);
+
+arr.unshift(11);
+console.log(arr);
+
+let num = [10,20,30,40,50,60];
+let newnum = num.slice(2,4);
+console.log(newnum);
+
+let names = ["alamgir","kamal","sameer","asif","ashik"];
+names.splice(1,2,"new names add");
+console.log(names);
 
 
 

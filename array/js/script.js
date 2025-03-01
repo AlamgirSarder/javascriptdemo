662dbbf71db8c5e007268694dc9885cb5854a26a
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-03-01 22:15:20 +0600

    add function

--- array/js/script.js
+++ array/js/script.js
@@ -61,3 +61,11 @@ const number = [10,20,30,40,50,60];
 const newNumber = number.map((eachnumber)=>{
 console.log(eachnumber*2);
 });
+
+
+
+const add = () =>{
+    console.log(`welcome arrow function`);
+    
+}
+add();

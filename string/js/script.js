0880336d34ddb5855bae7362750b75b221e5b0f6
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-03-01 23:46:38 +0600

    split

--- string/js/script.js
+++ string/js/script.js
@@ -8,6 +8,15 @@
     6. substring()
     7. toLowerCase()
     8. toUpperCase()
+    9. concat()
+    10. trim()
+    11. padStart()
+    12. padEnd()
+    13. toString()
+    14. repeat()
+    15. replace()
+    16. split()
+    17. indexOf()
 
 
 
@@ -42,4 +51,39 @@ console.log(len);
 
 names = "Alamgir";
 len = names.toUpperCase();
+console.log(len);
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ",text2);
+console.log(text3);
+
+
+names = "               Alamgir sarder         ";
+len = names.trim();
+console.log(len);
+
+names = "Al";
+len = names.padStart(4,"X");
+console.log(len);
+
+names = "Alamgir Sarder";
+len = names.repeat(4);
+console.log(len);
+
+names = "Alamgir Sarder";
+len = names.replace("Sarder","Kahn");
+console.log(len);
+
+// names = "my name is Alamgir Sarder";
+// len = names.split(" ");
+// console.log(len);
+
+// names = "my name is Alamgir Sarder";
+// len = names.split(",");
+// console.log(len);
+
+
+names = "my name is Alamgir Sarder";
+len = names.split(' ').join("-");
 console.log(len);

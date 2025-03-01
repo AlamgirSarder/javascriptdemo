7f29932882d47e6e47fa7ea68159adf422bd6394
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-03-01 23:08:52 +0600

    add string method

--- string/js/script.js
+++ string/js/script.js
@@ -1,12 +1,45 @@
-//String
+//String Methods
 /*
     1. length
-    2. 
+    2. charAt()
+    3. at()
+    4. charCodeAt()
+    5. slice();
+    6. substring()
+    7. toLowerCase()
+    8. toUpperCase()
+
 
 
 */
 
 
-const names = "Alamgir";
-const len = names.length;
+let names = "Alamgir";
+let len = names.length;
+console.log(len);
+
+let  text = "Hello Word";
+let char  = text.charCodeAt(0);
+console.log(char);
+
+
+ names = "Alamgir";
+ len = names.slice(2,5);
+console.log(len);
+
+ names = "Alamgir";
+ len = names.slice(2);
+console.log(len);
+
+
+names = "Alamgir";
+len = names.substring(2,5);
+console.log(len);
+
+names = "Alamgir";
+len = names.toLowerCase();
+console.log(len);
+
+names = "Alamgir";
+len = names.toUpperCase();
 console.log(len);

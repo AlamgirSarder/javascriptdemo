8696f623497c5ce9c22c9ad9e260297fc22b2989
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-03-02 12:55:00 +0600

    method add

--- array/js/script.js
+++ array/js/script.js
@@ -1,4 +1,18 @@
 // Array Methods
+/*
+    1. pop()
+    2. push()
+    3. shift()
+    4. unshift()
+    5. splice()
+    6. slice()
+    7. sort()
+    8. reverse()
+
+
+
+
+*/
 
 let arr = [12,34,61,1,56,78,800,90,26,,100,30,80];
 let max = arr[0];
@@ -9,3 +23,35 @@ for(let i = 1; i<=arr.length; i++){
     }
 }
 console.log(max);
+
+
+let names = ["Alamgir","Asif","Deopk"];
+console.log(names);
+
+names.pop();
+console.log(names);
+
+names.push("Sameer");
+console.log(names);
+
+names.shift();
+console.log(names);
+
+names.unshift("Alamgir","Kazi");
+console.log(names);
+
+names.splice(2,0,"Akash","Kamal","Raju");
+console.log(names);
+
+names.splice(4,1);
+console.log(names);
+
+
+let newnames =names.slice(1,3);
+console.log(newnames);//original array not change
+
+names.sort();
+console.log(names);
+
+names.reverse();
+console.log(names);

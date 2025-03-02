be3f743d6f237b86b637726e10a632356211c18e
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-03-02 12:12:50 +0600

    add array

--- array/js/script.js
+++ array/js/script.js
@@ -1,71 +1,11 @@
-//push()        //find()
-//pop()         //forEach()
-//shift()       //map()
-//unshift()
-//slice()
-//splice()
+// Array Methods
 
+let arr = [12,34,61,1,56,78,800,90,26,,100,30,80];
+let max = arr[0];
 
-let arr = [1,2,3,4,5,6,7,8];
-console.log(arr);
-
-arr.push(9);
-console.log(arr);
-
-arr.pop();
-console.log(arr);
-
-arr.shift();
-console.log(arr);
-
-arr.unshift(11);
-console.log(arr);
-
-let num = [10,20,30,40,50,60];
-let newnum = num.slice(2,4);
-console.log(newnum);
-
-let names = ["alamgir","kamal","sameer","asif","ashik"];
-names.splice(1,2,"new names add");
-console.log(names);
-
-
-let name = [10,20,25,50];
-const xx = name.find((x)=>{
-    if(x<20){
-        console.log(x);
+for(let i = 1; i<=arr.length; i++){
+    if(arr[i]>max){
+        max = arr[i];
     }
-})
-
-// const x = name.find(item=>item>20);
-// console.log(x);
-
-
-let a = [10,20,30,40,50,60];
-
-a.forEach((x)=>{
-        console.log(x);
-        
-})
-
-
-
-const y = [1,2,3,4,5];
-let z =y.map((x)=>{
-     return x*3;
-})
-
-console.log(z);
-
-const number = [10,20,30,40,50,60];
-const newNumber = number.map((eachnumber)=>{
-console.log(eachnumber*2);
-});
-
-
-
-const add = () =>{
-    console.log(`welcome arrow function`);
-    
 }
-add();
+console.log(max);

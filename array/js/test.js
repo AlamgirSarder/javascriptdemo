212b3bab7c4bb18706f3afe7534e766794a83b72
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-03-15 14:30:12 +0600

    Array Method list add

---
+++ array/js/test.js
@@ -0,0 +1,16 @@
+/*
+    JavaScript Array Medthods
+
+    1. sort()*           9. slice()              17. find()
+    2. reverse()         10. splice()            18. findIndex()
+    3. pop()             11. isArray()           19. includes()
+    4. push()            12. indexOf()           20. some()
+    5. shift()           13. lastIndexOf()       21. forEach()
+    6. unshift()         14. entries()           22. toString()
+    7. concat()          15. every()             23. valueOf()
+    8. join()            16. filter              23. fill()
+
+
+
+
+*/
